5a40c168925b8177f13462139d0475303c72ca18
brauck <brauckhoff@yandex.com>
2023-11-08 05:09:13 +0300

    make first bat

---
+++ lab2/bats/P1.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+md d:\30333_14\CX\c11\c111
+md d:\30333_14\CX\c11\c112
+md d:\30333_14\CX\c12\c121
+md d:\30333_14\CX\c12\c122
+copy con d:\30333_14\CX\c11\F11.ttt
+date /T >> d:\30333_14\CX\c11\F11.ttt
+time /T >> d:\30333_14\CX\c11\F11.ttt
+copy d:\30333_14\CX\c11\F11.ttt d:\30333_14\CX\c12\c122\F11c.ttt
