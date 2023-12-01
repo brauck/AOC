8fbda854f539ba086fe5a70332b9746735b78e31
brauck <brauckhoff@yandex.com>
2023-12-02 01:25:07 +0300

    add arrray

--- lab2/commands_parts/P3_array.bat
+++ lab2/commands_parts/P3_array.bat
@@ -1,7 +1,6 @@
-@ECHO off
+@echo off
 
 if not exist help.txt (help > help.txt)
-IF exist lines.bat (del lines.bat)
 
 set isArgument=false
 set /a arrLength=0
@@ -34,10 +33,10 @@ if not x%atr:/=%==x%atr% (
 ::where /q %1
 
 :: If the programm must exit with errorlevel 0 use this code
-::FOR /F usebackq %%F IN (`where %1`) DO (set command=%%F)
-::IF defined command (the same code)
+::for /f usebackq %%f in (`where %1`) do (set command=%%f)
+::if defined command (the same code)
 
-FOR /F usebackq %%F IN (`where %1`) DO (set command=%%F)
+for /f usebackq %%f in (`where %1`) do (set command=%%f)
 IF defined command (
   set isArgument=false  
   goto string 
@@ -45,11 +44,11 @@ IF defined command (
 :: Check if an attribute is a built in command to cmd.exe
 :cmd
 set str=
-FOR /F usebackq %%F IN (`findstr /b /i /c:"%1  " help.txt`) DO (
+for /f usebackq %%F in (`findstr /b /i /c:"%1  " help.txt`) do (
 set str=%%F
 )
 
-IF defined str (  
+if defined str (  
   set isArgument=false
   goto string
   ) else (  
@@ -79,7 +78,7 @@ set arr[%arrLength%]=%line%
 echo %arrLength%
 
 setlocal enabledelayedexpansion
-for /l %%n in (0,1,5) do ( 
+for /l %%n in (0,1,%arrLength%) do ( 
    echo !arr[%%n]! 
 )
 endlocal
