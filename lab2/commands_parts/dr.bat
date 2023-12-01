2bb8160e82dfe350f3cd3b6363e7d0d25da0e5c2
brauck <brauckhoff@yandex.com>
2023-12-02 00:59:09 +0300

    add arrray

--- lab2/commands_parts/dr.bat
+++ lab2/commands_parts/dr.bat
@@ -4,7 +4,9 @@ if not exist help.txt (help > help.txt)
 IF exist lines.bat (del lines.bat)
 
 set isArgument=false
+set /a arrLength=0
 set line=%1
+set arr[]=
 
 shift
 
@@ -29,17 +31,17 @@ if not x%atr:/=%==x%atr% (
 )
 
 :: The programm with this command may exit with errorlevel 1
-where /q %1
-
-IF %errorlevel% == 0 (
-  set isArgument=false  
-  goto string 
-  ) else (goto cmd)
+::where /q %1
 
 :: If the programm must exit with errorlevel 0 use this code
 ::FOR /F usebackq %%F IN (`where %1`) DO (set command=%%F)
 ::IF defined command (the same code)
 
+FOR /F usebackq %%F IN (`where %1`) DO (set command=%%F)
+IF defined command (
+  set isArgument=false  
+  goto string 
+  ) else (goto cmd)
 :: Check if an attribute is a built in command to cmd.exe
 :cmd
 set str=
@@ -63,15 +65,25 @@ if %isArgument% == true (
   goto exe
 )
 
-echo %line% >> lines.bat
+::echo %line% >> lines.bat
+set arr[%arrLength%]=%line%
+set /a arrLength+=1
 set line=%1
 shift
 goto exe
 
 :execute
-::echo last line
-echo %line% >> lines.bat
-echo pause >> lines.bat
+::insert last line
+::echo %line% >> lines.bat
+set arr[%arrLength%]=%line%
+echo %arrLength%
+
+setlocal enabledelayedexpansion
+for /l %%n in (0,1,5) do ( 
+   echo !arr[%%n]! 
+)
+endlocal
+
 echo %errorlevel%
 ::call lines.bat
 echo programm completed successful
