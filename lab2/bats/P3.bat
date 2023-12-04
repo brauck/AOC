ba7168f99b72b0f77f94dcb85d5b6c0a6a95a02a
brauck <brauckhoff@yandex.com>
2023-12-05 00:54:29 +0300

    modify p3

--- lab2/bats/P3.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:getprogramme
-if "%1"=="" goto end
-%1
-shift
-goto getprogramme
-:end
-echo work is end
--- lab2/bats/P3/P3_v2.bat
+++ lab2/bats/P3/P3_v2.bat
@@ -1,10 +1,11 @@
 @echo off
 
 if not exist help.txt (help > help.txt)
-if exist lines.bat (del lines.bat)
 
 set isArgument=false
+set /a arrLength=0
 set line=%1
+set arr[]=
 
 shift
 
@@ -12,7 +13,7 @@ shift
 :exe
 if "%1"=="" goto execute
 
-::Check if extension of argument is equal .exe
+::Check if extension of argument equal .exe
 set ifExe=.ext
 set ext=%~x1
 if defined ext (set ifExe=%ext%)
@@ -28,22 +29,20 @@ if not x%atr:/=%==x%atr% (
   goto string
 )
 
-where /q %1
+:: Quiet mode
+::where /q %1
 
-if %errorlevel% == 0 (
+set command=
+for /f usebackq %%f in (`where %1`) do (set command=%%f)
+IF defined command (
   set isArgument=false  
   goto string 
   ) else (goto cmd)
-
-:: Alternative way
-::for /f usebackq %%f in (`where %1`) do (set command=%%f)
-::if defined command (the same code)
-
-:: Check if an argument is a built in command to cmd.exe
+:: Check if an attribute is a built in command to cmd.exe
 :cmd
 set str=
-for /f usebackq %%f in (`findstr /b /i /c:"%1  " help.txt`) do (
-set str=%%f
+for /f usebackq %%F in (`findstr /b /i /c:"%1  " help.txt`) do (
+  set str=%%F
 )
 
 if defined str (  
@@ -62,16 +61,23 @@ if %isArgument% == true (
   goto exe
 )
 
-echo %line% >> lines.bat
+::echo %line% >> lines.bat
+set arr[%arrLength%]=%line%
+set /a arrLength+=1
 set line=%1
 shift
 goto exe
 
 :execute
-::Echo last line
-echo %line% >> lines.bat
-echo pause >> lines.bat
-echo %errorlevel%
-call lines.bat
-del help.txt
+::insert last line
+set arr[%arrLength%]=%line%
+echo %arrLength%
+
+setlocal enabledelayedexpansion
+for /l %%n in (0,1,%arrLength%) do ( 
+   echo !arr[%%n]!
+   !arr[%%n]!
+)
+endlocal
+
 echo programm completed successful
