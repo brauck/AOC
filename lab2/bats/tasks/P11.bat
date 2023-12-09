b08e76cd3b6789d8c26a352d139f780253cdd204
brauck <brauckhoff@yandex.com>
2023-12-09 16:04:41 +0300

    correct p11

--- lab2/bats/tasks/P11.bat
+++ lab2/bats/tasks/P11.bat
@@ -8,11 +8,11 @@ set /a count+=1
 if %count% GTR 5 goto :eof
 if [%1]==[] goto :eof
 set attr=%~a1
-if not defined attr (
-  echo Папка %1 не найдена
+if defined attr (
+  dir %1
   shift
   goto loop
 )
-dir %1
+echo Папка %1 не найдена
 shift
 goto loop
